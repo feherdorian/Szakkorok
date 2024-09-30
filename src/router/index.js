ab6fa0055df0dcea9649c9b780a6bdd7ce8d51e8
feherdorian <94679613+feherdorian@users.noreply.github.com>
2024-09-30 08:16:34 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/szakkorok',
-      name: 'szakkorok',
-      component: () => import('../views/Szakkorok.vue')
-    },
+    }
   ]
 })
 
